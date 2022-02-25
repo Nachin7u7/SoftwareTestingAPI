e002c58e310e8fa89e9537f0376658d86660182e
Ricardo Valencia <ignacio.valencia.sauma@gmail.com>
2022-02-25 02:03:56 -0400

    HomeWork DONE

--- src/test/java/crudItems/CRUDItems.java
+++ src/test/java/crudItems/CRUDItems.java
@@ -10,68 +10,29 @@ import io.restassured.response.Response;
 
 public class CRUDItems {
 	@Test
-    public void crudProject(){
-        // Create
-        JSONObject body = new JSONObject();
-        body.put("Content","AndreItem");
+	public void crudProject() {
+		JSONObject body = new JSONObject();
+		body.put("Content", "Item");
 
-        Response response=given()
-                .auth()
-                .preemptive()
-                .basic("upb_api@api.com","12345")
-                .body(body.toString())
-                .log().all()
-        .when()
-                .post("https://todo.ly/api/items.json");
+		Response response = given().auth().preemptive().basic("upb@api.com", "51231").body(body.toString()).log().all()
+				.when().post("https://todo.ly/api/items.json");
 
-        response.then()
-                .statusCode(200)
-                .body("Content",equalTo("AndreItem"))
-                .log().all();
+		response.then().statusCode(200).body("Content", equalTo("Item")).log().all();
 
-        int idProject =response.then().extract().path("Id");
+		int idProject = response.then().extract().path("Id");
 
-        // Read
-        response=given()
-                .auth()
-                .preemptive()
-                .basic("upb_api@api.com","12345")
-                .log().all()
-        .when()
-                .get("https://todo.ly/api/items/"+idProject+".json");
+		response = given().auth().preemptive().basic("upb@api.com", "51231").log().all().when()
+				.get("https://todo.ly/api/items/" + idProject + ".json");
 
-        response.then()
-                .statusCode(200)
-                .body("Content",equalTo("AndreItem"))
-                .log().all();
-        // Update
-        body.put("Checked",true);
-        response=given()
-                .auth()
-                .preemptive()
-                .basic("upb_api@api.com","12345")
-                .body(body.toString())
-                .log().all()
-        .when()
-                .put("https://todo.ly/api/items/"+idProject+".json");
+		response.then().statusCode(200).body("Content", equalTo("Item")).log().all();
+		body.put("Checked", true);
+		response = given().auth().preemptive().basic("upb@api.com", "51231").body(body.toString()).log().all().when()
+				.put("https://todo.ly/api/items/" + idProject + ".json");
 
-        response.then()
-                .statusCode(200)
-                .body("Content",equalTo("AndreItem"))
-                .body("Checked",equalTo(true))
-                .log().all();
-        // Delete
-        response=given()
-                .auth()
-                .preemptive()
-                .basic("upb_api@api.com","12345")
-                .log().all()
-        .when()
-                .delete("https://todo.ly/api/items/"+idProject+".json");
+		response.then().statusCode(200).body("Content", equalTo("Item")).body("Checked", equalTo(true)).log().all();
+		response = given().auth().preemptive().basic("upb@api.com", "51231").log().all().when()
+				.delete("https://todo.ly/api/items/" + idProject + ".json");
 
-        response.then()
-                .statusCode(200)
-                .body("Content",equalTo("AndreItem"))
-                .log().all();
-    }
+		response.then().statusCode(200).body("Content", equalTo("Item")).log().all();
+	}
 }
